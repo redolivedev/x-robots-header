ff0b23e74a91d40d97c3b5fc476a64c5302ea3be
Michael Bonner <michael@redolive.com>
2019-06-11 10:03:25 -0600

    add admin notice

--- includes/class-x-robots-header.php
+++ includes/class-x-robots-header.php
@@ -11,20 +11,25 @@ class X_Robots_Header
 			)
 		);
 
-		function sample_admin_notice__success()
-		{
-			?>
-		<div class="notice notice-warning">
-			<p><?= 'This site will not be indexed by Google. Disable the X Robots Tag plugin to allow Google to index the site' ?></p>
-		</div>
-	<?php
-}
-add_action('admin_notices', 'sample_admin_notice__success');
-}
+		add_action(
+			'admin_notices',
+			array(
+				$this,
+				'addAdminNotice'
+			)
+		);
+	}
 
-public function sendHeaders($headers)
-{
-	$headers['X-Robots-Tag'] = 'noindex, nofollow';
-	return $headers;
-}
+	public function sendHeaders($headers)
+	{
+		$headers['X-Robots-Tag'] = 'noindex, nofollow';
+		return $headers;
+	}
+
+	public function addAdminNotice()
+	{
+		echo '<div class="notice notice-warning">
+			<p>This site will not be indexed by Google. Disable the X Robots Tag plugin to allow Google to index the site</p>
+		</div>';
+	}
 }
